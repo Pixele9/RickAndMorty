4f20507ebfe86b9c7103b7c0e4307f0f26904709
Pixele9 <pixele9@gmail.com>
2020-09-06 08:25:08 -0500

    *added* search bar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import Navbar from "./components/navbar";
 
 export default function App() {
   const [characters, setCharacters] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
-    getAPI().then((resp) => setCharacters(resp.results));
+    getAPI().then((resp) => {
+      setCharacters(resp.results);
+    });
   }, []);
+
+  const filteredChars = characters.filter((char) => {
+    return char.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
+  });
 
   const getAPI = async () => {
     const url = "https://rickandmortyapi.com/api/character/";
@@ -18,8 +25,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      {characters.map((character) => {
+      <Navbar value={searchValue} onChange={(value) => setSearchValue(value)} />
+      {filteredChars.map((character) => {
         return (
           <Character
             key={character.id}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div className="navbar">
       <span>Search Character: </span>
-      <input className="searchbar" />
+      <input
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+        className="searchbar"
+      />
     </div>
   );
 }
